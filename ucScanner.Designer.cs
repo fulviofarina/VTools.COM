c433f9d5446e9a80e6fe0006a16d981dfb771cb8
fulviofarina <fulviofarina@gmail.com>
2016-09-06 23:43:43 -0400

    v1.0

---
+++ ucScanner.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace VTools
+{
+    partial class ucScanner
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ucScanner));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.baudsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.baudbox = new System.Windows.Forms.ToolStripTextBox();
+            this.dataBitsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataBitsBox = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.open = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.close = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.comBox = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.status = new System.Windows.Forms.ToolStripLabel();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel2,
+            this.toolStripSeparator4,
+            this.open,
+            this.toolStripSeparator2,
+            this.close,
+            this.toolStripSeparator1,
+            this.toolStripLabel1,
+            this.comBox,
+            this.toolStripSeparator3,
+            this.status});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1010, 57);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.baudsToolStripMenuItem,
+            this.dataBitsToolStripMenuItem});
+            this.toolStripLabel2.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(112, 54);
+            this.toolStripLabel2.Text = "SCANNER";
+            // 
+            // baudsToolStripMenuItem
+            // 
+            this.baudsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.baudbox});
+            this.baudsToolStripMenuItem.Name = "baudsToolStripMenuItem";
+            this.baudsToolStripMenuItem.Size = new System.Drawing.Size(156, 30);
+            this.baudsToolStripMenuItem.Text = "Bauds";
+            // 
+            // baudbox
+            // 
+            this.baudbox.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.baudbox.Name = "baudbox";
+            this.baudbox.Size = new System.Drawing.Size(152, 33);
+            this.baudbox.Text = "9600";
+            this.baudbox.TextBoxTextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.baudbox.TextChanged += new System.EventHandler(this.Box_TextChanged);
+            // 
+            // dataBitsToolStripMenuItem
+            // 
+            this.dataBitsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dataBitsBox});
+            this.dataBitsToolStripMenuItem.Name = "dataBitsToolStripMenuItem";
+            this.dataBitsToolStripMenuItem.Size = new System.Drawing.Size(156, 30);
+            this.dataBitsToolStripMenuItem.Text = "DataBits";
+            // 
+            // dataBitsBox
+            // 
+            this.dataBitsBox.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold);
+            this.dataBitsBox.Name = "dataBitsBox";
+            this.dataBitsBox.Size = new System.Drawing.Size(152, 33);
+            this.dataBitsBox.Text = "8";
+            this.dataBitsBox.TextBoxTextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.dataBitsBox.Click += new System.EventHandler(this.Box_TextChanged);
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(6, 57);
+            // 
+            // open
+            // 
+            this.open.BackColor = System.Drawing.Color.Honeydew;
+            this.open.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.open.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.open.Image = ((System.Drawing.Image)(resources.GetObject("open.Image")));
+            this.open.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(66, 54);
+            this.open.Text = "OPEN";
+            this.open.Click += new System.EventHandler(this.open_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 57);
+            // 
+            // close
+            // 
+            this.close.BackColor = System.Drawing.Color.Linen;
+            this.close.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.close.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.close.Image = ((System.Drawing.Image)(resources.GetObject("close.Image")));
+            this.close.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(70, 54);
+            this.close.Text = "CLOSE";
+            this.close.Click += new System.EventHandler(this.close_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 57);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.BackColor = System.Drawing.Color.LemonChiffon;
+            this.toolStripLabel1.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(108, 54);
+            this.toolStripLabel1.Text = "COM PORT";
+            // 
+            // comBox
+            // 
+            this.comBox.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comBox.Name = "comBox";
+            this.comBox.Size = new System.Drawing.Size(100, 57);
+            this.comBox.Text = "38";
+            this.comBox.TextBoxTextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.comBox.TextChanged += new System.EventHandler(this.Box_TextChanged);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 57);
+            // 
+            // status
+            // 
+            this.status.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(76, 54);
+            this.status.Text = "STATUS";
+            // 
+            // ucScanner
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "ucScanner";
+            this.Size = new System.Drawing.Size(1010, 57);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton open;
+        private System.Windows.Forms.ToolStripButton close;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripTextBox comBox;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripLabel status;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripLabel2;
+        private System.Windows.Forms.ToolStripMenuItem baudsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox baudbox;
+        private System.Windows.Forms.ToolStripMenuItem dataBitsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox dataBitsBox;
+    }
+}
---
+++ ucScanner.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace VTools
+{
+    public partial class ucScanner : UserControl
+    {
+        private System.IO.Ports.SerialPort serial = null;
+
+        public ucScanner()
+        {
+            InitializeComponent();
+
+            this.open.Enabled = true;
+            this.close.Enabled = false;
+        }
+
+        private EventHandler callBack = null;
+
+        public void MakeScanner(ref EventHandler cBack)
+        {
+            string com = "COM" + this.comBox.Text;
+            int Baudrate = Convert.ToInt32(this.baudbox.Text);
+            int bits = Convert.ToInt32(this.dataBitsBox.Text);
+            try
+            {
+                serial = new System.IO.Ports.SerialPort(com, Baudrate, System.IO.Ports.Parity.None, bits, System.IO.Ports.StopBits.One);
+                serial.Handshake = System.IO.Ports.Handshake.None;
+                serial.ReceivedBytesThreshold = 1;
+                serial.RtsEnable = true;
+                serial.DataReceived += serial_DataReceived;
+                this.callBack = cBack;
+            }
+            catch (Exception ex)
+            {
+                this.status.Text = "Problems: " + ex.Message;
+            }
+        }
+
+        private string result = string.Empty;
+
+        private string stat = string.Empty;
+
+        public string Status
+        {
+            get { return stat; }
+            set { stat = value; }
+        }
+
+        public string Result
+        {
+            get { return result; }
+            set { result = value; }
+        }
+
+        private void serial_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            result = string.Empty;
+            stat = "algo";
+
+            try
+            {
+                //  this.status.Text = "Data Received";
+
+                while (!string.IsNullOrEmpty(stat))
+                {
+                    stat = serial.ReadExisting();
+                    result += stat;
+                }
+
+                serial.DiscardInBuffer();
+                serial.DiscardOutBuffer();
+
+                callBack.Invoke(sender, new EventArgs());
+            }
+            catch (Exception ex)
+            {
+                stat = "Problems: " + ex.Message;
+                //    this.statuslbl.Text = ex.Message + "\t\t" + ex.StackTrace;
+            }
+        }
+
+        /* COPIA PORSIAAAAAAA
+         *
+         *  private void serial_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            result = string.Empty;
+            stat = string.Empty;
+
+            try
+            {
+              //  this.status.Text = "Data Received";
+
+                result = serial.ReadExisting();
+
+                serial.DiscardInBuffer();
+                serial.DiscardOutBuffer();
+
+                callBack.Invoke(sender, new EventArgs());
+            }
+            catch (Exception ex)
+            {
+                stat = "Problems: " + ex.Message;
+                //    this.statuslbl.Text = ex.Message + "\t\t" + ex.StackTrace;
+            }
+        }
+         * */
+
+        //   bool openPort;
+
+        public bool OpenPort
+        {
+            set
+            {
+                if (true == value)
+                {
+                    open_Click(null, EventArgs.Empty);
+                }
+            }
+        }
+
+        //    bool closePort;
+
+        public bool ClosePort
+        {
+            set
+            {
+                if (true == value)
+                {
+                    close_Click(null, EventArgs.Empty);
+                }
+            }
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (this.serial.IsOpen) serial.Close();
+                serial.Open();
+                this.open.Enabled = !serial.IsOpen;
+                this.close.Enabled = serial.IsOpen;
+
+                if (this.serial.IsOpen) this.status.Text = "Waiting for Scan";
+                else this.status.Text = "Could not OPEN the connection";
+            }
+            catch (Exception ex)
+            {
+                this.status.Text = "Problems: " + ex.Message;
+            }
+        }
+
+        private void close_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (this.serial.IsOpen)
+                {
+                    serial.Close();
+                }
+                this.open.Enabled = !this.serial.IsOpen;
+                this.close.Enabled = serial.IsOpen;
+
+                if (this.serial.IsOpen) this.status.Text = "Connection still opened";
+                else this.status.Text = "Connection CLOSED";
+            }
+            catch (Exception ex)
+            {
+                this.status.Text = "Problems: " + ex.Message;
+            }
+        }
+
+        private void Box_TextChanged(object sender, EventArgs e)
+        {
+            string com = ((ToolStripTextBox)sender).Text;
+            if (string.IsNullOrWhiteSpace(com)) return;
+            if (string.IsNullOrEmpty(com)) return;
+
+            close_Click(sender, e);
+
+            try
+            {
+                if (sender.Equals(this.comBox))
+                {
+                    com = "COM" + com;
+                    this.serial.PortName = com;
+                }
+                else if (sender.Equals(this.baudbox))
+                {
+                    int Baudrate = Convert.ToInt32(com);
+                    this.serial.BaudRate = Baudrate;
+                }
+                else if (sender.Equals(this.dataBitsBox))
+                {
+                    int bits = Convert.ToInt32(com);
+                    this.serial.DataBits = bits;
+                }
+            }
+            catch (Exception ex)
+            {
+                this.status.Text = "Problems: " + ex.Message;
+            }
+
+            open_Click(sender, e);
+        }
+    }
+}
